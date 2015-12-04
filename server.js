de19caa96fa88c3f8c84e20ff75206e011a53f89
Shrey <shrey_vithalani@yahoo.com>
2015-12-04 14:59:39 -0800

    changes finally working

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var Hapi = require('hapi');
 var server = new Hapi.Server();
 var bell = require('bell');
-server.connection({ port: 3000 });
+
 var Joi = require('joi');
 
 
 
-
+server.connection({ port: 3000 });
 
 var Boom = require("boom");
  
@@ -20,7 +20,6 @@ var dbOpts = {
 };
 
 
-
 server.register({
     register: require('hapi-mongodb'),
     options: dbOpts
@@ -32,47 +31,17 @@ server.register({
 });
 
 
-const scheme = function (server, options) {
-
-    return {
-        payload: function (request, reply) {
-
-           console.log(request);
-        }
-    };
-};
-
-server.auth.scheme('custom', scheme);
-server.auth.strategy('default', 'custom');
-server.auth.default('default');
-
-
-
-
-function checkPassword(data) {
-	var return_string;
-	if(data.password === data.password_confirm) {
-		return_string = "confirmed password";
-	}
-	else {
-		return_string = "password does not match";
-	}
-	return return_string;
-}
-
-  
 server.register(require('inert'), function (err) {
     if (err) {
         throw err;
     }
-
     server.route({
         method: 'GET',
         path: '/',
         handler: function (request, reply) {
             console.log(request);
             reply.file('./public/hello.html');
-        }
+        }        
     });
      server.route({
         method: 'GET',
@@ -80,16 +49,14 @@ server.register(require('inert'), function (err) {
         handler: function (request, reply) {
             reply.file('./public/register.html');
         }
+
     });
       server.route({
         method: 'GET',
-        path: '/login',
-        config: {
-        auth: 'default',
+        path: '/login',        
         handler: function (request, reply) {
-
-            console.log(request);
-        }
+            reply.file('./public/login.html')
+            //console.log(request);
         }
        
     });
@@ -120,8 +87,6 @@ server.register(require('inert'), function (err) {
 
               db.collection('users').findOne({  "name" : data.username }, function(err, result) {
                 if (err) return reply(Boom.internal('Internal MongoDB error', err));
-                //reply(result);
-                //console.log(result);
                 if(result == null){
                     db.collection('users').insert(doc);
                     reply.redirect('/login');
@@ -136,12 +101,12 @@ server.register(require('inert'), function (err) {
         }
     });
 
+
+
 });
+    
 
 
-
-      
-       
 
 
 server.start(function () {
